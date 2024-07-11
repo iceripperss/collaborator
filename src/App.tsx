a7c7a253b5245880bf182773c191ed4052871997
Peter Hryhoruk <lifeofzombie6@gmail.com>
2024-07-11 22:15:26 +0300

    css loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logo from "./logo";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
       </header>
     </div>
   );
