98444bc5374b912e3202ef8dea52e9115bddc58b
iceripperss <43995999+iceripperss@users.noreply.github.com>
2024-07-15 11:17:36 +0000

    Deploying to gh-pages from @ iceripperss/collaborator@bd158530414c9b6ef248e560524e1882a2cdaf97 🚀

--- components/ListGroup/Filters/Filters.js
+++ components/ListGroup/Filters/Filters.js
@@ -12,33 +12,44 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { ticketsActions } from "@redux/ticket/reducer";
 import classes from "./Filters.module.scss";
-var flights = [
-    { label: "Без пересадок", value: 0 },
-    { label: "1 Пересадка", value: 1 },
-    { label: "2 Пересадки", value: 2 },
-    { label: "3 Пересадки", value: 3 },
+var FlightType;
+(function (FlightType) {
+    FlightType[FlightType["NoTransfers"] = 0] = "NoTransfers";
+    FlightType[FlightType["OneTransfer"] = 1] = "OneTransfer";
+    FlightType[FlightType["TwoTransfers"] = 2] = "TwoTransfers";
+    FlightType[FlightType["ThreeTransfers"] = 3] = "ThreeTransfers";
+})(FlightType || (FlightType = {}));
+var flightsTransfers = [
+    { label: "Без пересадок", value: FlightType.NoTransfers },
+    { label: "1 Пересадка", value: FlightType.OneTransfer },
+    { label: "2 Пересадки", value: FlightType.TwoTransfers },
+    { label: "3 Пересадки", value: FlightType.ThreeTransfers },
 ];
+var Checkbox = function (_a) {
+    var label = _a.label, value = _a.value, checked = _a.checked, onChange = _a.onChange;
+    return (_jsxs("label", { className: classes.filter, children: [_jsx("input", { name: label, checked: checked, className: classes.filter__input, type: "checkbox", value: value, onChange: onChange }), label] }));
+};
 export var Filters = function () {
     var _a = React.useState([]), filters = _a[0], setFilters = _a[1];
+    var _b = React.useState(false), all = _b[0], setAll = _b[1];
     var dispatch = useDispatch();
     var handleFilterChange = function (e) {
-        var value = Number(e.target.value);
-        var allFilter = document.getElementById("allFilter");
-        allFilter.checked = false;
-        setFilters(e.target.checked ? __spreadArray(__spreadArray([], filters, true), [value], false) : filters.filter(function (filter) { return filter !== value; }));
+        var value = parseInt(e.target.value, 10);
+        var changedFilters = e.target.checked ? __spreadArray(__spreadArray([], filters, true), [value], false) : filters.filter(function (filter) { return filter !== value; });
+        setFilters(changedFilters);
+        setAll(changedFilters.length === flightsTransfers.length);
     };
-    var handleAllFilters = function (e) {
-        return setFilters(e.target.checked ? flights.map(function (_a) {
-            var value = _a.value;
-            return value;
-        }) : []);
+    var toggleAllFilters = function (e) {
+        var changedFilters = e.target.checked ? Object.values(FlightType) : [];
+        setFilters(changedFilters);
+        setAll(function (prev) { return !prev; });
     };
     useEffect(function () {
         dispatch(ticketsActions.filterByTransfers(filters));
     }, [dispatch, filters]);
-    return (_jsxs("aside", { className: classes.aside, children: [_jsx("h3", { className: classes.aside__header, children: "\u041A\u0456\u043B\u044C\u043A\u0456\u0441\u0442\u044C \u043F\u0435\u0440\u0435\u0441\u0430\u0434\u043E\u043A" }), _jsxs("div", { className: classes.aside__filters, children: [_jsxs("label", { className: classes.aside__filters__filter, children: [_jsx("input", { id: "allFilter", name: "all", className: classes.aside__filters__filter__input, type: "checkbox", onChange: handleAllFilters }), "\u0412\u0441\u0456"] }), flights.map(function (_a) {
+    return (_jsxs("aside", { className: classes.aside, children: [_jsx("h3", { className: classes.aside__header, children: "\u041A\u0456\u043B\u044C\u043A\u0456\u0441\u0442\u044C \u043F\u0435\u0440\u0435\u0441\u0430\u0434\u043E\u043A" }), _jsxs("div", { className: classes.aside__filters, children: [_jsx(Checkbox, { checked: all, label: "\u0412\u0441\u0456", onChange: toggleAllFilters }), flightsTransfers.map(function (_a) {
                         var value = _a.value, label = _a.label;
-                        return (_jsxs("label", { className: classes.aside__filters__filter, children: [_jsx("input", { name: label, checked: filters.includes(value), className: classes.aside__filters__filter__input, type: "checkbox", value: value, onChange: handleFilterChange }), label] }, label));
+                        return (_jsx(Checkbox, { checked: filters.includes(value), value: value, label: label, onChange: handleFilterChange }, label));
                     })] })] }));
 };
 //# sourceMappingURL=Filters.js.map
